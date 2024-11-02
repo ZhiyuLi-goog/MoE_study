e175794d44d9fa422ffcd5828e78ae86d1581552
Zhiyu Li <lizhiyu@google.com>
2024-11-02 22:54:54 +0000

    2d sharding

--- clm/model_utils_tpu.py
+++ clm/model_utils_tpu.py
@@ -37,7 +37,7 @@ logger = logging.get_logger(__name__)
 
 
 def prepare_model(model, config):
-    if config.tensor_parallelism == 1:
+    if config.tensor_parallelism == 0:
 
         def shard_output(output, mesh):
             real_output = None
