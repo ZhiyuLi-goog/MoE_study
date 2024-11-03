a53c765a8a17dee8807db18650e8c8e0e74da2cd
Zhiyu Li <lizhiyu@google.com>
2024-11-03 21:23:22 +0000

    2d shard

--- clm/model_utils_tpu.py
+++ clm/model_utils_tpu.py
@@ -220,8 +220,8 @@ def setup_model_optimizer(config):
         for layer in model.model.layers:
             layer.self_attn.rotary_emb._set_buffer(device=xm.xla_device())
     logger.info("model loaded")
-    # model = prepare_model(model, config)
-    # model = model.to(dtype)
+    model = prepare_model(model, config)
+    model = model.to(dtype)
     logger.info("model prepared")
     gc.collect()
     xm.mark_step()
