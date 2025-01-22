dbd0ccf4f59a1c01008cfff74900051755084734
Qinwen Xu <qinwen@google.com>
2025-01-22 00:55:25 +0000

    update

--- clm/verify.py
+++ clm/verify.py
@@ -123,9 +123,9 @@ def main(config: DictConfig):
         labels = batch.pop("labels")
         outputs = model(**batch)
         logits = outputs.logits
-        xm.add_step_closure(print_tensor, args=('logits', logits[:1], -1))
+        xm.add_step_closure(print_tensor, args=('logits', logits[:1], None))
         for i, layer_output in enumerate(outputs.hidden_states):
-            xm.add_step_closure(print_tensor, args=(f'layer_output_{i}', layer_output[:1], -1))
+            xm.add_step_closure(print_tensor, args=(f'layer_output_{i}', layer_output[:1], None))
         
         xm.mark_step()
         logger.info(f"{outputs.logits}")
